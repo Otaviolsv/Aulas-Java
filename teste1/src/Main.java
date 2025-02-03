a733797efd5821ea5078754422cd734757245eaf
Otaviolsv <143139884+Otaviolsv@users.noreply.github.com>
2025-02-03 08:06:50 -0300

    Add files via upload

---
+++ teste1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        String nome1 = "Otavio";
+//        String sobrenome1 = "Vasconcelos";
+
+        System.out.printf("\nnome:%s  sobrenome:%s\n", "otavio", "Vasconcelos" );
+
+        System.out.println("\n\"teste\"");
+    }
+}
